21efd10b4413a60429ea42bbd70ad8fd071c2a4c
anasyakubu <yakubuanas04@gmail.com>
2024-06-16 20:37:57 +0100

    Seach config

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,13 @@
 
 // import Nav from "../../Components/Nav";
 import Hero from "../../Section/Hero/Hero";
+import Footer from "../../Section/Footer/Footer";
 
 const Home = () => {
   return (
     <div className="Home bg-[#1b1b1b] text-[#aeaeae]">
       <Hero />
+      <Footer />
     </div>
   );
 };
--- src/Section/Hero/Hero.jsx
+++ src/Section/Hero/Hero.jsx
@@ -8,12 +8,32 @@ const Hero = () => {
       <div className="p-24">
         <div className="">
           <div className="">
-            <h2 className="text-4xl text-[#ece383]">Get. Updated.</h2>
+            <h2 className="text-4xl text-[#ece383]">Github Rader.</h2>
             <p className="mt-3">
-              Gain access to articles, tutorials, and resources that are not
-              available anywhere else. <br />
+              Search more than 51M users on Github. A database that contain all
+              the Github Profiles <br />
             </p>
           </div>
+          <div className="mt-5">
+            <form>
+              <input
+                className="w-[70%] p-4 rounded-full outline-none border border-black text-black"
+                type="text"
+                name=""
+                id=""
+                placeholder="Search for User."
+              />
+              <div className="mt-5">
+                <button
+                  type="submit"
+                  className="p-3 w-36 pr-6 pl-6 rounded-full bg-[#f8f8f8] text-black text-sm hover:bg-transparent border border-[#f8f8f8] hover:text-[#f8f8f8] hover:border-[#f8f8f8]"
+                >
+                  Search
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="mt-10"></div>
         </div>
       </div>
     </div>
